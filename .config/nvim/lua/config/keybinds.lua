a6ed8f9bd60b89e379f2d419bcb3bc51c6a8084e
Bilal <bjlegend007@gmail.com>
2025-05-29 13:16:28 +0530

    added keymap for copy and paste

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,2 +1,11 @@
+-- LEADER KEY
 vim.g.mapleader = " "
+
+
+-- key bindings
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+
+-- bindings for overriding the copy and paste with system one
+vim.keymap.set("v", "y", '"+y')
+vim.keymap.set("n", "p", '"+p')
