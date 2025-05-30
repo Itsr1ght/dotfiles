ac11a884aa9ee86391adfae416c759b30e43c260
Bilal <bjlegend007@gmail.com>
2025-05-30 11:36:59 +0530

    updated commit and tried to added keybind for terminal for exiting

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 -- bindings for overriding the copy and paste with system one
 vim.keymap.set("v", "y", '"+y')
 vim.keymap.set("n", "p", '"+p')
+
+
+-- bindings for terminal
+vim.keymap.set("t", "<leader>/", "q<esc>")
