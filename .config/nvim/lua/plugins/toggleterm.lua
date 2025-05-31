de823b5882dbcac79cfa9c7b343fa539a13f87d1
Bilal <bjlegend007@gmail.com>
2025-05-31 17:56:58 +0530

    fixed the terminal open and close problem

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -4,12 +4,15 @@ return {
         version = "*",
         config = function()
             require("toggleterm").setup {
-                shade_terminals = false
+                shade_terminals = false,
+                on_open = function(term)
+                    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-/>", "exit<CR>", { noremap = true, silent = true })
+                end,
             }
         end,
         keys = {
             {
-                "<leader>/",
+                "<C-/>",
                 ":ToggleTerm<cr>",
                 desc = "Toggle The Terminal with /"
             }
