183d83e11134d614050a20a24cc59f10ffe4711f
Bilal <bjlegend007@gmail.com>
2025-05-31 18:07:47 +0530

    updated the options and added keymaps for visual lines

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 -- bindings for overriding the copy and paste with system one
 vim.keymap.set("v", "y", '"+y')
 vim.keymap.set("n", "p", '"+p')
+
+-- Indent and reselect visual selection
+vim.keymap.set("v", "<", "<gv", { desc = "Outdent and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent and reselect" })
