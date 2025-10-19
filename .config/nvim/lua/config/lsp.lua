cc3e76e77ca609d1c53efbcdf47c55225ee477f1
Bilal <bjlegend007@gmail.com>
2025-10-19 17:33:23 +0530

    added the zls and updated the lsp.lua
    * added the zls lamgauge server
    *

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,16 +1,18 @@
-vim.lsp.enable('lua_ls')
+vim.lsp.enable({'lua_ls', 'zls'})
 
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.opt.completeopt = {
-                'menu', 'menuone', 'noinsert', 'fuzzy', 'popup'
-            }
-            vim.lsp.completion.enable(true, client.id, ev.buff, {autotrigger = true})
-            vim.keymap.set('i', '<C-Space>', function()
-               vim.lsp.completion.get()
-            end)
+        if client then
+            if client:supports_method('textDocument/completion') then
+                vim.opt.completeopt = {
+                    'menu', 'menuone', 'noinsert', 'fuzzy', 'popup'
+                }
+                vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
+                vim.keymap.set('i', '<C-Space>', function()
+                   vim.lsp.completion.get()
+                end)
+            end
         end
     end
 })
