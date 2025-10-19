d40eaf66170313703545ae1a3596c8af257c12b1
Bilal <bjlegend007@gmail.com>
2025-10-19 15:09:37 +0530

    added comment

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,5 +1,7 @@
+-- Setting main leader to space
 vim.g.mapleader = " "
 
+-- Setting the tab size to 4 and enabling number
 vim.o.number = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,3 +1,4 @@
 local nv = vim;
 
+-- keymap for explorer as 'cd'
 nv.keymap.set('n', '<leader>cd', function() nv.cmd('e .') end)
