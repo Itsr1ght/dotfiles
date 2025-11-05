6f7a623d0d258dd3e607dfa9eb767fd1147fe98e
Bilal <bjlegend007@gmail.com>
2025-11-05 15:50:39 +0530

    fixed the pylsp

--- .config/nvim/lsp/pylsp.lua
+++ .config/nvim/lsp/pylsp.lua
@@ -1,9 +1,19 @@
 return {
     cmd = {'pylsp'},
     filetypes = {'python'},
-    root_markers = {'setup.py', 'pyproject.toml', 'setup.cfg', 'requirements.txt'},
+    root_markers = {'setup.py', 'pyproject.toml', 'setup.cfg', 'requirements.txt', '.git'},
     settings = {
         pylsp = {
+            plugins = {
+                jedi_completion = { enabled = true },
+                jedi_hover = { enabled = true },
+                jedi_references = { enabled = true },
+                jedi_signature_help = { enabled = true },
+                jedi_symbols = { enabled = true },
+                pyflakes = { enabled = true },
+                pycodestyle = { enabled = true },
+                mccabe = { enabled = true },
+            }
         }
     }
 }
