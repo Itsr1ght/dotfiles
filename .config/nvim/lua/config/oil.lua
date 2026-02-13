479f36034426df57f59efe64fbc05bec69fb539d
Bilal <bjlegend007@gmail.com>
2026-02-13 19:55:59 +0530

    updated the oil configuration

--- .config/nvim/lua/config/oil.lua
+++ .config/nvim/lua/config/oil.lua
@@ -2,6 +2,20 @@ vim.pack.add({
     "https://github.com/stevearc/oil.nvim",
 })
 
-require("oil").setup()
+require("oil").setup({
+    columns = {
+        "icon",
+        "size",
+        "mtime",
+    },
+    view_options = {
+        show_hidden = true,
+        natural_order = "fast",
+        preview_win = {
+            update_on_cursor_moved = true,
+            preview_method = "load",
+        }
+    }
+})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
