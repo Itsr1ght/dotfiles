42bb759fed115c8ec1a04dc5e21f9c394464f34d
Bilal <bjlegend007@gmail.com>
2026-03-10 23:28:44 +0530

    added config for wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.enable_tab_bar = false
+config.front_end = 'WebGpu'
+config.animation_fps = 60
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+--config.window_decorations = "RESIZE"
+
+return config
